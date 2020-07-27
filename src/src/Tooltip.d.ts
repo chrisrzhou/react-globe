ab562b1481e47bda2924790f86e9e16790f178bf
chrisrzhou <chrisrzhou@pm.me>
2020-07-26 23:16:38 -0700

    remote emitted declarations file

--- src/src/Tooltip.d.ts
+++
@@ -1,9 +0,0 @@
-import { Instance } from 'tippy.js';
-export default class Tooltip {
-  instance: Instance;
-  div: HTMLDivElement;
-  constructor(div: HTMLDivElement);
-  destroy(): void;
-  hide(): void;
-  show(clientX: number, clientY: number, content: string): void;
-}
--- src/src/useResize.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-import { Size } from './types';
-export default function useResize<T>(
-  mountRef: React.RefObject<HTMLDivElement>,
-  initialSize?: Size,
-): Size;
